c36935316e5796479671d61be314a65da5dc868f
Rong Ou <rong.ou@gmail.com>
2024-10-21 16:40:56 -0700

    build once and split outputs

--- recipe/setup.py
+++ recipe/setup.py
@@ -8,7 +8,7 @@ args, headers, etc for pytorch.
 Read more at the pytorch docs:
 https://pytorch.org/docs/stable/cpp_extension.html#torch.utils.cpp_extension.CUDAExtension
 """
-import os
+
 import pathlib
 
 from setuptools import setup, find_packages
@@ -16,11 +16,11 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 _this_dir = pathlib.Path(__file__).parent.absolute()
 
-# Check environment variable to determine which component to build
-build_target = os.environ.get("BUILD_FLASH_ATTN_COMPONENT", "core")
-
-if build_target == "core":
-    ext_modules = [
+setup(
+    packages=find_packages(
+        include=["flash_attn*"],
+    ),
+    ext_modules=[
         CUDAExtension(
             name="flash_attn_2_cuda",
             sources=[
@@ -138,9 +138,6 @@ if build_target == "core":
                 _this_dir / "csrc" / "cutlass" / "include",
             ],
         ),
-    ]
-elif build_target == "fused_dense_lib":
-    ext_modules = [
         CUDAExtension(
             name="fused_dense_lib",
             sources=[
@@ -152,69 +149,66 @@ elif build_target == "fused_dense_lib":
                 'nvcc': ['-O3'],
             },
         ),
-    ]
-elif build_target == "layer_norm":
-    ext_modules = [
         CUDAExtension(
             name="dropout_layer_norm",
             sources=[
-                "cscr/layer_norm/ln_api.cpp",
-                "cscr/layer_norm/ln_fwd_256.cu",
-                "cscr/layer_norm/ln_bwd_256.cu",
-                "cscr/layer_norm/ln_fwd_512.cu",
-                "cscr/layer_norm/ln_bwd_512.cu",
-                "cscr/layer_norm/ln_fwd_768.cu",
-                "cscr/layer_norm/ln_bwd_768.cu",
-                "cscr/layer_norm/ln_fwd_1024.cu",
-                "cscr/layer_norm/ln_bwd_1024.cu",
-                "cscr/layer_norm/ln_fwd_1280.cu",
-                "cscr/layer_norm/ln_bwd_1280.cu",
-                "cscr/layer_norm/ln_fwd_1536.cu",
-                "cscr/layer_norm/ln_bwd_1536.cu",
-                "cscr/layer_norm/ln_fwd_2048.cu",
-                "cscr/layer_norm/ln_bwd_2048.cu",
-                "cscr/layer_norm/ln_fwd_2560.cu",
-                "cscr/layer_norm/ln_bwd_2560.cu",
-                "cscr/layer_norm/ln_fwd_3072.cu",
-                "cscr/layer_norm/ln_bwd_3072.cu",
-                "cscr/layer_norm/ln_fwd_4096.cu",
-                "cscr/layer_norm/ln_bwd_4096.cu",
-                "cscr/layer_norm/ln_fwd_5120.cu",
-                "cscr/layer_norm/ln_bwd_5120.cu",
-                "cscr/layer_norm/ln_fwd_6144.cu",
-                "cscr/layer_norm/ln_bwd_6144.cu",
-                "cscr/layer_norm/ln_fwd_7168.cu",
-                "cscr/layer_norm/ln_bwd_7168.cu",
-                "cscr/layer_norm/ln_fwd_8192.cu",
-                "cscr/layer_norm/ln_bwd_8192.cu",
-                "cscr/layer_norm/ln_parallel_fwd_256.cu",
-                "cscr/layer_norm/ln_parallel_bwd_256.cu",
-                "cscr/layer_norm/ln_parallel_fwd_512.cu",
-                "cscr/layer_norm/ln_parallel_bwd_512.cu",
-                "cscr/layer_norm/ln_parallel_fwd_768.cu",
-                "cscr/layer_norm/ln_parallel_bwd_768.cu",
-                "cscr/layer_norm/ln_parallel_fwd_1024.cu",
-                "cscr/layer_norm/ln_parallel_bwd_1024.cu",
-                "cscr/layer_norm/ln_parallel_fwd_1280.cu",
-                "cscr/layer_norm/ln_parallel_bwd_1280.cu",
-                "cscr/layer_norm/ln_parallel_fwd_1536.cu",
-                "cscr/layer_norm/ln_parallel_bwd_1536.cu",
-                "cscr/layer_norm/ln_parallel_fwd_2048.cu",
-                "cscr/layer_norm/ln_parallel_bwd_2048.cu",
-                "cscr/layer_norm/ln_parallel_fwd_2560.cu",
-                "cscr/layer_norm/ln_parallel_bwd_2560.cu",
-                "cscr/layer_norm/ln_parallel_fwd_3072.cu",
-                "cscr/layer_norm/ln_parallel_bwd_3072.cu",
-                "cscr/layer_norm/ln_parallel_fwd_4096.cu",
-                "cscr/layer_norm/ln_parallel_bwd_4096.cu",
-                "cscr/layer_norm/ln_parallel_fwd_5120.cu",
-                "cscr/layer_norm/ln_parallel_bwd_5120.cu",
-                "cscr/layer_norm/ln_parallel_fwd_6144.cu",
-                "cscr/layer_norm/ln_parallel_bwd_6144.cu",
-                "cscr/layer_norm/ln_parallel_fwd_7168.cu",
-                "cscr/layer_norm/ln_parallel_bwd_7168.cu",
-                "cscr/layer_norm/ln_parallel_fwd_8192.cu",
-                "cscr/layer_norm/ln_parallel_bwd_8192.cu",
+                "csrc/layer_norm/ln_api.cpp",
+                "csrc/layer_norm/ln_fwd_256.cu",
+                "csrc/layer_norm/ln_bwd_256.cu",
+                "csrc/layer_norm/ln_fwd_512.cu",
+                "csrc/layer_norm/ln_bwd_512.cu",
+                "csrc/layer_norm/ln_fwd_768.cu",
+                "csrc/layer_norm/ln_bwd_768.cu",
+                "csrc/layer_norm/ln_fwd_1024.cu",
+                "csrc/layer_norm/ln_bwd_1024.cu",
+                "csrc/layer_norm/ln_fwd_1280.cu",
+                "csrc/layer_norm/ln_bwd_1280.cu",
+                "csrc/layer_norm/ln_fwd_1536.cu",
+                "csrc/layer_norm/ln_bwd_1536.cu",
+                "csrc/layer_norm/ln_fwd_2048.cu",
+                "csrc/layer_norm/ln_bwd_2048.cu",
+                "csrc/layer_norm/ln_fwd_2560.cu",
+                "csrc/layer_norm/ln_bwd_2560.cu",
+                "csrc/layer_norm/ln_fwd_3072.cu",
+                "csrc/layer_norm/ln_bwd_3072.cu",
+                "csrc/layer_norm/ln_fwd_4096.cu",
+                "csrc/layer_norm/ln_bwd_4096.cu",
+                "csrc/layer_norm/ln_fwd_5120.cu",
+                "csrc/layer_norm/ln_bwd_5120.cu",
+                "csrc/layer_norm/ln_fwd_6144.cu",
+                "csrc/layer_norm/ln_bwd_6144.cu",
+                "csrc/layer_norm/ln_fwd_7168.cu",
+                "csrc/layer_norm/ln_bwd_7168.cu",
+                "csrc/layer_norm/ln_fwd_8192.cu",
+                "csrc/layer_norm/ln_bwd_8192.cu",
+                "csrc/layer_norm/ln_parallel_fwd_256.cu",
+                "csrc/layer_norm/ln_parallel_bwd_256.cu",
+                "csrc/layer_norm/ln_parallel_fwd_512.cu",
+                "csrc/layer_norm/ln_parallel_bwd_512.cu",
+                "csrc/layer_norm/ln_parallel_fwd_768.cu",
+                "csrc/layer_norm/ln_parallel_bwd_768.cu",
+                "csrc/layer_norm/ln_parallel_fwd_1024.cu",
+                "csrc/layer_norm/ln_parallel_bwd_1024.cu",
+                "csrc/layer_norm/ln_parallel_fwd_1280.cu",
+                "csrc/layer_norm/ln_parallel_bwd_1280.cu",
+                "csrc/layer_norm/ln_parallel_fwd_1536.cu",
+                "csrc/layer_norm/ln_parallel_bwd_1536.cu",
+                "csrc/layer_norm/ln_parallel_fwd_2048.cu",
+                "csrc/layer_norm/ln_parallel_bwd_2048.cu",
+                "csrc/layer_norm/ln_parallel_fwd_2560.cu",
+                "csrc/layer_norm/ln_parallel_bwd_2560.cu",
+                "csrc/layer_norm/ln_parallel_fwd_3072.cu",
+                "csrc/layer_norm/ln_parallel_bwd_3072.cu",
+                "csrc/layer_norm/ln_parallel_fwd_4096.cu",
+                "csrc/layer_norm/ln_parallel_bwd_4096.cu",
+                "csrc/layer_norm/ln_parallel_fwd_5120.cu",
+                "csrc/layer_norm/ln_parallel_bwd_5120.cu",
+                "csrc/layer_norm/ln_parallel_fwd_6144.cu",
+                "csrc/layer_norm/ln_parallel_bwd_6144.cu",
+                "csrc/layer_norm/ln_parallel_fwd_7168.cu",
+                "csrc/layer_norm/ln_parallel_bwd_7168.cu",
+                "csrc/layer_norm/ln_parallel_fwd_8192.cu",
+                "csrc/layer_norm/ln_parallel_bwd_8192.cu",
             ],
             extra_compile_args={
                 "cxx": ["-O3"],
@@ -229,21 +223,13 @@ elif build_target == "layer_norm":
                     "--expt-relaxed-constexpr",
                     "--expt-extended-lambda",
                     "--use_fast_math",
-                ],
+                ]
             },
             include_dirs=[
                 _this_dir / "csrc" / "layer_norm",
             ],
         ),
-    ]
-else:
-    raise ValueError(f"Unknown build target: {build_target}")
-
-setup(
-    packages=find_packages(
-        include=["flash_attn*"],
-    ),
-    ext_modules=ext_modules,
+    ],
     cmdclass={"build_ext": BuildExtension},
     zip_safe=False,
 )
