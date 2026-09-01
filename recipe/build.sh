7173dace924c8b699d53931cf1cf12245b1d5ec8
Daniel Ching <dching@nvidia.com>
2026-09-01 13:32:45 -0500

    BUG: Use correct path for patchelf command

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ python -m pip install . -vvv --no-deps --no-build-isolation
 # dynamically link to libpython because the python interpreter has already statically linked
 # libpython, so all the symbols are already loaded into the process.
 patchelf --remove-needed "libpython${PY_VER}.so.1.0" \
-    lib/python*/site-packages/{flash_attn_2_cuda,fused_dense_lib,dropout_layer_norm}.cpython-*.so
+    ${PREFIX}/lib/python*/site-packages/{flash_attn_2_cuda,fused_dense_lib,dropout_layer_norm}.cpython-*.so
